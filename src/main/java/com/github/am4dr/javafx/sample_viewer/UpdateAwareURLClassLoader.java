d7171fcf0722a0c05960c14a4453c068527190a4
amadarain <t.f.octo@gmail.com>
2018-07-07 11:57:34 +0900

    use FileUpdatePublisher on UpdateAwareURLClassLoader

--- src/main/java/com/github/am4dr/javafx/sample_viewer/UpdateAwareURLClassLoader.java
+++ src/main/java/com/github/am4dr/javafx/sample_viewer/UpdateAwareURLClassLoader.java
@@ -1,26 +1,19 @@
 package com.github.am4dr.javafx.sample_viewer;
 
-import com.github.am4dr.javafx.sample_viewer.internal.DaemonThreadFactory;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.file.*;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
-import java.util.concurrent.SubmissionPublisher;
 
 import static com.github.am4dr.javafx.sample_viewer.internal.UncheckedFunction.uncheckedFunction;
 
 public final class UpdateAwareURLClassLoader extends URLClassLoader {
 
-    private final ExecutorService executor = Executors.newCachedThreadPool(DaemonThreadFactory.INSTANCE);
-    private final SubmissionPublisher<Path> publisher = new SubmissionPublisher<>(executor, 10);
-    private final WatchService watchService;
+    private final FileUpdatePublisher publisher = new FileUpdatePublisher();
 
     public UpdateAwareURLClassLoader() {
         this(List.of());
@@ -28,41 +21,14 @@ public final class UpdateAwareURLClassLoader extends URLClassLoader {
     public UpdateAwareURLClassLoader(List<Path> paths) {
         super(new URL[0]);
         paths.stream().map(uncheckedFunction(path -> path.toUri().toURL())).forEach(this::addURL);
-        try {
-            watchService = FileSystems.getDefault().newWatchService();
-            take(watchService);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void take(WatchService watchService) {
-        executor.submit(() -> {
-            while (!Thread.interrupted()) {
-                final WatchKey key;
-                try {
-                    key = watchService.take();
-                } catch (InterruptedException e) {
-                    break;
-                }
-                key.pollEvents().stream()
-                        .map(WatchEvent::context)
-                        .map(Path.class::cast)
-                        .forEach(publisher::submit);
-                key.reset();
-            }
-        });
     }
 
     @Override
     public void close() throws IOException {
         super.close();
-        publisher.close();
-        executor.shutdownNow();
-        watchService.close();
+        publisher.shutdown();
     }
 
-    private final List<Path> paths = new ArrayList<>();
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
         synchronized (getClassLoadingLock(name)) {
@@ -77,11 +43,7 @@ public final class UpdateAwareURLClassLoader extends URLClassLoader {
                 final Path path = Paths.get(aClass.getProtectionDomain().getCodeSource().getLocation().toURI())
                         .resolve(aClass.getName().replace(".", "/") + ".class")
                         .toRealPath();
-                final Path parent = path.getParent();
-                if (!paths.contains(parent)) {
-                    paths.add(parent);
-                    parent.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
-                }
+                publisher.addDirectory(path.getParent());
             } catch (URISyntaxException | IOException e) {
                 e.printStackTrace();
             }
